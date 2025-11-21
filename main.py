244ec6334b110582322722a5c3007847e0799b8c
Mohd Talha <talhamoh017@gmail.com>
2025-11-21 21:47:24 +0530

    Data Ingestion Final Implementation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcome to the text summarizer project")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
